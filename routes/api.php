d8b2735334da4b4a1253881ad2e31937b562e7dd
aliabdulameer <aliabdulameerk@gmail.com>
2024-05-31 17:23:30 +0330

    12:20 1/6/2024

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+///
 Route::get('/user', function (Request $request) {
     return Auth::user()->load('roles');
 })->middleware('auth:sanctum');
